e8db079bac6c3b1091bab2b61019c8b2059a86d9
Edwin Sam <edwinsamjr@gmail.com>
2022-02-16 16:26:47 -0500

    Submitting Ordering, Limiting, Grouping

--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/15_newest_park.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/15_newest_park.sql
@@ -1,3 +1,8 @@
 -- 15. The name and date established of the newest national park.
 -- (1 row)
 
+SELECT park_name, date_established
+FROM park
+GROUP BY park_name, date_established
+ORDER BY date_established DESC
+LIMIT 1;
