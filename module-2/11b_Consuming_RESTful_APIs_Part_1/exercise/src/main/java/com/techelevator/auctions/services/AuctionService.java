cc674d46dc154484ef1937dbb1f8bdedadd635ef
Edwin Sam <edwinsamjr@gmail.com>
2022-02-28 14:13:09 -0500

    Submitting Restful APIs Pt. 1

--- module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -7,7 +7,7 @@ import com.techelevator.auctions.model.Auction;
 public class AuctionService {
 
     public static final String API_BASE_URL = "http://localhost:3000/auctions/";
-    private final RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
 
     public Auction[] getAllAuctions() {
